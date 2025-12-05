0c94c92b978d277536bd35dbb6e47421e43c88d4
Jordi Pulido <jordi.pulido@adevinta.com>
2025-12-05 15:39:35 +0100

    Upgrade zim version

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,2 +1,4 @@
-zimfw() { source /Users/jordi.pulido/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
-zmodule() { source /Users/jordi.pulido/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
+# FILE AUTOMATICALLY GENERATED FROM /Users/jordi.pulido/.zimrc
+# EDIT THE SOURCE FILE AND THEN RUN zimfw build. DO NOT DIRECTLY EDIT THIS FILE!
+
+if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "${HOME}/.dotfiles/shell/zsh/.zim/zimfw.zsh" "${@}" }
