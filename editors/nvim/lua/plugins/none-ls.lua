48c878d9e5cab06edbf341db968b4448cdac95b4
Jordi Pulido <jpulidofontalba@gmail.com>
2026-06-12 15:53:50 +0200

    fix(nvim): define ktfmt as custom none-ls formatter (no builtin exists)

--- editors/nvim/lua/plugins/none-ls.lua
+++ editors/nvim/lua/plugins/none-ls.lua
@@ -2,11 +2,25 @@ return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
+    local helpers = require("null-ls.helpers")
+
+    local ktfmt = helpers.make_builtin({
+      name = "ktfmt",
+      meta = { url = "https://github.com/facebook/ktfmt" },
+      method = null_ls.methods.FORMATTING,
+      filetypes = { "kotlin" },
+      generator_opts = {
+        command = "ktfmt",
+        args = { "--kotlinlang-style", "-" },
+        to_stdin = true,
+      },
+      factory = helpers.formatter_factory,
+    })
 
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.ktfmt,
+        ktfmt,
         null_ls.builtins.formatting.yamlfmt.with({ filetypes = { "yaml", "yaml.github" } }),
         null_ls.builtins.diagnostics.yamllint,
       },
