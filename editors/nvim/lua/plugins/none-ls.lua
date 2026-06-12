cc14fa16f5b92b5da8c1a44338589f7f839e8060
Jordi Pulido <jpulidofontalba@gmail.com>
2026-06-12 15:52:43 +0200

    fix(nvim): replace ktlint with ktfmt, remove duplicate keymap

--- editors/nvim/lua/plugins/none-ls.lua
+++ editors/nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,15 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.ktlint,
-	        null_ls.builtins.formatting.yamlfmt.with({ filetypes = { "yaml", "yaml.github" } }),
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.ktfmt,
+        null_ls.builtins.formatting.yamlfmt.with({ filetypes = { "yaml", "yaml.github" } }),
         null_ls.builtins.diagnostics.yamllint,
-			},
-		})
-
-		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-	end,
+      },
+    })
+  end,
 }
